79cdcaa6766c92bc74c9beaf722ff71159c14da5
ChinmayBhat-em23 <chinmaybhat1904@gmail.com>
2024-12-26 09:38:13 +0530

    Update questions.ts

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -20,7 +20,7 @@ export const questions: Question[] = [
   {
     id: 3,
     text: "What is 10 ÷ 2?",
-    difficulty: "medium",
+    difficulty: "easy",
     correctAnswer: "5",
     imageUrl: "https://images.unsplash.com/photo-1635372722656-389f87a941b7?auto=format&fit=crop&q=80&w=800",
     audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/PinkPanther30.wav"
@@ -28,7 +28,7 @@ export const questions: Question[] = [
   {
     id: 4,
     text: "What is 8 × 7?",
-    difficulty: "medium",
+    difficulty: "easy",
     correctAnswer: "56",
     imageUrl: "https://images.unsplash.com/photo-1518133910546-b6c2fb7d79e3?auto=format&fit=crop&q=80&w=800",
     audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/StarWars60.wav"
@@ -36,11 +36,251 @@ export const questions: Question[] = [
   {
     id: 5,
     text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+  {
+    id: 6,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 7,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 8,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 9,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 10,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 11,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 12,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 13,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 14,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 15,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 16,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 17,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 18,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 19,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 20,
+    text: "What is 12 × 12?",
+    difficulty: "easy",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 21,
+    text: "What is 12 × 12?",
     difficulty: "hard",
     correctAnswer: "144",
     imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
     audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
-  }
+  },
+{
+    id: 22,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 23,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 24,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 25,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 26,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 27,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 28,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 29,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 30,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 31,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 32 ,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 33,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 34,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
+{
+    id: 35,
+    text: "What is 12 × 12?",
+    difficulty: "hard",
+    correctAnswer: "144",
+    imageUrl: "https://images.unsplash.com/photo-1635070041078-e363dbe005cb?auto=format&fit=crop&q=80&w=800",
+    audioUrl: "https://www2.cs.uic.edu/~i101/SoundFiles/gettysburg10.wav"
+  },
 ];
 
 // Helper functions
